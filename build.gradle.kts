7c9a4b3a32443958d6e98a4bbbc5e17410f3fbca
rickysixx <51907568+rickysixx@users.noreply.github.com>
2023-07-14 07:59:59 +0200

    Add Gradle JAXB plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("com.intershop.gradle.jaxb") version "6.0.0"
 }
 
 group = "dev.rickysixx"
@@ -10,9 +11,21 @@ repositories {
 }
 
 dependencies {
+    // JAXB dependencies
+    implementation("jakarta.xml.bind:jakarta.xml.bind-api:4.0.0")
+    implementation("org.glassfish.jaxb:jaxb-runtime:4.0.3")
+
     implementation("info.picocli:picocli:4.7.4")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+jaxb {
+    javaGen {
+        register("pipeline") {
+            schema = file("xsd/PipelineXML.xsd")
+        }
+    }
 }
