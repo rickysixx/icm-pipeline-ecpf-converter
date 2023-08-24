abf91b7ca517281ffc34c1de8837b345a1c7a69c
rickysixx <51907568+rickysixx@users.noreply.github.com>
2023-08-24 17:46:30 +0200

    Add method to visit a generic node.

--- src/main/java/dev/rickysixx/ecpf/pipeline/NodeVisitor.java
+++ src/main/java/dev/rickysixx/ecpf/pipeline/NodeVisitor.java
@@ -1,6 +1,7 @@
 package dev.rickysixx.ecpf.pipeline;
 
 import de.intershop.core._2010.Parameter;
+import de.intershop.pipeline._2010.Node;
 import de.intershop.pipeline._2010.StartNode;
 import dev.rickysixx.ecpf.io.IndentingPrintWriter;
 
@@ -12,9 +13,27 @@ public class NodeVisitor
     private final Pipeline pipeline;
     private final IndentingPrintWriter outputWriter;
 
+    private void dispatchVisit(Node node)
+    {
+        throw new UnsupportedOperationException(String.format("Visit method for node type [%s] has not been implemented yet.", node.getClass().getSimpleName()));
+    }
+
     public NodeVisitor(Pipeline pipeline, PrintWriter outputWriter)
     {
         this.pipeline = pipeline;
         this.outputWriter = new IndentingPrintWriter(outputWriter);
+    }
+
+    public void visit(Node node)
+    {
+        outputWriter.println("{");
+
+        outputWriter.indentedBlock(() -> {
+            outputWriter.printf("type: [%s]\n", node.getClass().getSimpleName());
+
+            dispatchVisit(node);
+        });
+
+        outputWriter.println("}");
     }
 }
