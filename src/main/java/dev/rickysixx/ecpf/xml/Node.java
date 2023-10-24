069c4e929e9d701a8362abc07110dd5d6b41cc3a
rickysixx <51907568+rickysixx@users.noreply.github.com>
2023-10-24 14:17:24 +0200

    Add parsing of pipeline node definition XML file.

--- src/main/java/dev/rickysixx/ecpf/xml/Node.java
+++ src/main/java/dev/rickysixx/ecpf/xml/Node.java
@@ -1,11 +1,24 @@
 package dev.rickysixx.ecpf.xml;
 
+import com.intershop.pipeline.ObjectFactory;
+import com.intershop.pipeline.PipelineNodeDefinition;
+import com.intershop.pipeline.PipelineNodeTypes;
 import de.intershop.pipeline._2010.*;
+import dev.rickysixx.ecpf.PipelineEcpfConverter;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBElement;
+import jakarta.xml.bind.JAXBException;
 import jakarta.xml.bind.Unmarshaller;
 import jakarta.xml.bind.annotation.XmlTransient;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Stream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 @XmlTransient
 public abstract class Node
@@ -13,11 +26,55 @@ public abstract class Node
     @XmlTransient
     private Node parent;
 
+    @XmlTransient
+    private PipelineNodeDefinition pipelineNodeDefinition;
+
+    @SuppressWarnings("unchecked")
+    public PipelineNodeDefinition getPipelineNodeDefinition()
+    {
+        if (this instanceof PipelineNodeNode pipelineNode)
+        {
+            String[] splitHref = pipelineNode.getPipelet().getHref().split("/");
+            String cartridgeName = splitHref[1];
+            String pipelineNodeDefinitionQualifiedName = splitHref[3];
+            int lastIndexOfDot = pipelineNodeDefinitionQualifiedName.lastIndexOf('.');
+            String pipelineNodeDefinitionPath = pipelineNodeDefinitionQualifiedName.substring(0, lastIndexOfDot)
+               .replaceAll("\\.", "/")
+               .concat(pipelineNodeDefinitionQualifiedName.substring(lastIndexOfDot));
+            File systemCartridgesDir = PipelineEcpfConverter.getInstance().getSystemCartridgesDir();
+
+            try (ZipFile cartridgeJarFile = new ZipFile(Paths.get(systemCartridgesDir.getPath(), cartridgeName, "release", "lib", cartridgeName + ".jar").toFile()))
+            {
+                ZipEntry pipelineNodeDefinitionEntry = cartridgeJarFile.getEntry(pipelineNodeDefinitionPath);
+
+                try (InputStream pipelineNodeDefinition = cartridgeJarFile.getInputStream(pipelineNodeDefinitionEntry))
+                {
+                    JAXBContext jaxbContext = JAXBContext.newInstance(ObjectFactory.class);
+                    Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+                    JAXBElement<PipelineNodeDefinition> xmlElement = (JAXBElement<PipelineNodeDefinition>) unmarshaller.unmarshal(pipelineNodeDefinition);
+
+                    return xmlElement.getValue();
+                }
+                catch (JAXBException e)
+                {
+                    throw new RuntimeException(e);
+                }
+            }
+            catch (IOException e)
+            {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return null;
+    }
+
     private void afterUnmarshal(Unmarshaller unmarshaller, Object parent)
     {
         if (parent instanceof Node node)
         {
             this.parent = node;
+            this.pipelineNodeDefinition = getPipelineNodeDefinition();
         }
     }
 
